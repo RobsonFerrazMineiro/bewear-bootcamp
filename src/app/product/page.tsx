c79746b7c108b47d7b9d99dfa362cafec5c2c221
Robson Ferraz Mineiro <robson_r_f@hotmail.com>
2025-08-05 00:06:10 -0300

    chore: update shadcn theme

--- src/app/product/page.tsx
+++ src/app/product/page.tsx
@@ -4,7 +4,7 @@ const ProductPage = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center bg-gray-100">
       <h1 className="text-color-primary flex text-6xl">Product Page</h1>
-      <Button className="bg-green-500 text-white hover:bg-green-600">
+      <Button variant="default" className="mt-4">
         Click Me
       </Button>
     </div>
